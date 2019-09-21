cd09ade14753053a37abaddd7f20be1c2d56af98
arjunramesh99 <arjunramesh@utexas.edu>
2019-09-21 12:33:59 -0500

    Ignored files

--- lab3.d
+++
@@ -1,8 +0,0 @@
-.\lab3.o: Lab3.c
-.\lab3.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\lab3.o: ST7735.h
-.\lab3.o: PLL.h
-.\lab3.o: ../inc/tm4c123gh6pm.h
-.\lab3.o: Plot.h
-.\lab3.o: ClockBmp.h
-.\lab3.o: Timers.h
--- timers.d
+++
@@ -1,5 +0,0 @@
-.\timers.o: Timers.c
-.\timers.o: Timers.h
-.\timers.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\timers.o: ../inc/tm4c123gh6pm.h
-.\timers.o: PLL.h
